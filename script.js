33e6415fc09f53750c0e151e9e4d03f21a13f7f9
sentrisentri <kirubel.mulat@gmail.com>
2023-01-21 01:48:07 +0000

    pp

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function myFunction() {
-    document.getElementById("shadowBox").innerHTML = "Hello World";
-  }
+const form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  const selectedValue = document.querySelector('input[name="route"]:checked').value;
+  
+  localStorage.setItem("selectedRoute", selectedValue);
+});
